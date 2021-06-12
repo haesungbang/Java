ab81b507593a7623b7a1196fbaf8ae93e0ad1ebf
방해성 <haesunbang94@gmail.com>
2021-06-12 23:37:23 +0900

    JavaClass Day02

--- JavaClass/Chapter5/src/classpart/Student.java
+++ JavaClass/Chapter5/src/classpart/Student.java
@@ -8,11 +8,21 @@ public class Student {
 	int grade;
 	String address;
 	
+	// 반환값이 없는 경우 void 를 사용한다. 
 	public void showStudentInfo() {
 		System.out.println(studentName + ", " + address);
 	}
+	// 메서드 
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
+	}
 	// 이것을 사용해서 클래스를 사용해보자.
 	// 얘는 method 가 아니다.
+	
 	public static void main(String[] args) {
 	    Student studentLee = new Student();
 	    studentLee.studentName = "이순신";
