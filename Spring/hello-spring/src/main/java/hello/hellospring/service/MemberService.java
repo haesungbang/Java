69e1b8910419ba593a3f347cec4eeba64d3d01a3
banghaesung <haesungbang94@gmail.com>
2021-07-02 03:33:21 +0900

    Spring 회원 관리 예제 finished JSP 부터 해야겠다.

---
+++ Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    // 회원 서비스를 하려면 repository 가 있어야한다.
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복회원 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // cmd+option+m 으로 메서드로 뽑는다.
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
